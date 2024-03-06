ae58f1bffe49bc5fbde91cb8710ef2070970ebd5
evert <evert@DESKTOP-UQTPDK5>
2024-03-06 19:36:33 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade1/Controllers/ConvidadoController.cs
@@ -0,0 +1,99 @@
+﻿using Atividade1;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Atividade1.Controllers
+{
+    public class ConvidadoController : Controller
+    {
+        private static List<Convidado> convidados = new List<Convidado>();
+
+        public IActionResult Index()
+        {
+            return View(convidados);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public bool retoronecomparecimento()
+        {
+            return true;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Convidado convidado)
+        {
+            if (ModelState.IsValid)
+            {
+                convidados.Add(convidado);
+                convidado.Id = convidados.Select(cat => cat.Id).Max() + 1;
+                return RedirectToAction("Index");
+            }
+            return View(convidado);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var convidado = convidados.FirstOrDefault(c => c.Id == id);
+            if (convidado == null)
+            {
+                return NotFound();
+            }
+            return View(convidado);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var convidado = convidados.FirstOrDefault(c => c.Id == id);
+            if (convidado == null)
+            {
+                return NotFound();
+            }
+            return View(convidado);
+        }
+        public IActionResult Edit(int id, Convidado convidado)
+        {
+            if (id != convidado.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var existingConvidado = convidados.FirstOrDefault(c => c.Id == id);
+                if (existingConvidado != null)
+                {
+                    existingConvidado.Nome = convidado.Nome;
+                    existingConvidado.EMail = convidado.EMail;
+                    existingConvidado.Telefone = convidado.Telefone;
+                    existingConvidado.Comparecimento = convidado.Comparecimento;
+                }
+                return RedirectToAction("Index");
+            }
+            return View(convidado);
+        }
+
+        public IActionResult comparecimento()
+        {
+            bool resultado = retoronecomparecimento();
+
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var convidado = convidados.FirstOrDefault(c => c.Id == id);
+            if (convidado != null)
+            {
+                convidados.Remove(convidado);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Atividade1/Convidado.cs
@@ -0,0 +1,11 @@
+﻿namespace Atividade1
+{
+    public class Convidado
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string EMail { get; set; }
+        public string Telefone { get; set; }
+        public bool Comparecimento { get; set; }
+    }
+}
